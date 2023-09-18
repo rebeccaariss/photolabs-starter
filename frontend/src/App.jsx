4eae41481894e0337885827db1d603092c017d87
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-09-18 14:49:12 -0400

    Create nav bar by grouping components in parent nav component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -14,7 +14,7 @@ const App = () => {
           <PhotoListItem key={index} sampleData={photo} />
         ))
       } */}
-      <TopicList />
+      <TopNavigation />
     </div>
   );
 };
