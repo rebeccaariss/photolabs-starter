8ab21e3285abde32bf9a93c6842c43b4f76eb648
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-09-21 17:50:18 -0400

    Add custom useApplicationData hook (re: separation of concerns)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import useApplicationData from './hooks/useApplicationData';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import './App.scss';
@@ -8,27 +8,15 @@ import photos from 'mocks/photos';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const [ selectedPhoto, setSelectedPhoto ] = useState(null);
-  const [ modalState, setModalState ] = useState(false);
-  const [ favouritePhotos, setFavouritePhotos ] = useState([]);
-
-  const renderModal = (id) => {
-    setModalState(true);
-    setSelectedPhoto(id);
-  };
-
-  const showModal = (id) => modalState ?
-  setModalState(false)
-  : renderModal(id);
-
-  const showFavourites = (id) => favouritePhotos.includes(id) 
-    ? setFavouritePhotos(favouritePhotos.filter(photoId => photoId !== id)) // if the id is 
-    // already present in favouritePhotos array, this creates a new array with all photos that
-    // do not match the id in question, effectively updating state.
-    : setFavouritePhotos([...favouritePhotos, id]); // if the id is not yet included in the
-    // favouritePhotos array, the spread operator (...) copies the existing state array and
-    // re-establishes state with the existing values and the additional id on call to
-    // showFavourites. (call comes from onClick handler in PhotoFavButton component).
+  const {
+    state: { 
+      selectedPhoto, 
+      modalState, 
+      favouritePhotos 
+    },
+    showModal,
+    showFavourites
+  } = useApplicationData();
 
   return (
     <div className="App">
