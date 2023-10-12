3c86b8b0a4a6569a614ef95d35db762dd6fbe51d
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-10-12 18:02:22 -0400

    Add skeleton for user profile image, create separate divs for conditional rendering based on state

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -23,26 +23,30 @@ const PhotoListItem = (props) => {
 
   return (
     <li id={id} className="photo-list__item">
-      <PhotoFavButton id={id} favouritePhotos={favouritePhotos} showFavourites={showFavourites}/>
-      { photosAvailable ? 
-        <img src={urls.regular} className="photo-list__image" alt="main-photo" onClick={() => showModal(id)}/>
-        :
-        <Loading alt="main-photo-loading"/>
+      { photosAvailable ?
+        <div>
+          <PhotoFavButton id={id} favouritePhotos={favouritePhotos} showFavourites={showFavourites}/>
+          <img src={urls.regular} className="photo-list__image" alt="main-photo" onClick={() => showModal(id)}/>
+          <div className="photo-list__user-details">
+            <img src={user.profile} className="photo-list__user-profile" alt="profile-picture"/>
+            <div className="photo-list__user-info">
+              <p>{user.name}</p>
+              <p className="photo-list__user-location">{location.city}, {location.country}</p>
+            </div>
+          </div>
+        </div>
+      :
+        <div>
+          <Loading alt="main-photo-loading"/>
+          <div className="photo-list__user-details">
+            <Skeleton alt="profile-picture-loading" variant="circular" width={40} height={40} borderRadius={100}/>
+              <div>
+                <Skeleton alt="user-info-loading" variant="text" sx={{ fontSize: "1rem" }} height={12} width={80}/>
+                <Skeleton alt="user-location-loading" variant="text" sx={{ fontSize: "1rem" }} height={12} width={100}/>
+              </div>
+          </div>
+        </div>
       }
-      <div className="photo-list__user-details">
-        <img src={user.profile} className="photo-list__user-profile" alt="profile-picture"/>
-        { photosAvailable ?
-          <div className="photo-list__user-info">
-            <p>{user.name}</p>
-            <p className="photo-list__user-location">{location.city}, {location.country}</p>
-          </div>
-          :
-          <div>
-            <Skeleton variant="text" sx={{ fontSize: "1rem" }} height={12} width={80}/>
-            <Skeleton variant="text" sx={{ fontSize: "1rem" }} height={12} width={100}/>
-          </div>
-        }
-      </div>
     </li>
   );
 };
